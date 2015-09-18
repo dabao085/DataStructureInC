e0af18b2fa5d3b19055c11ebd49a00ed103a7c9b
Cheng Xiang <chengxiang085@gmail.com>
2015-09-18 22:47:23 +0800

    Update graph.h
    图的表示以及一些基本功能的实现。

--- graph.h
+++ graph.h
@@ -24,3 +24,60 @@ typedef struct Graph
 	AdjList adjList;
 	int numVertexes, numEdges;
 }GraphList;
+
+
+
+void CreateGraph(GraphList *g)
+{
+	int i,j,k;
+	EdgeNode *e;
+	EdgeNode *f;
+	printf("Enter the number of vertex and edges:\n");
+	scanf("%d,%d",&g->numVertexes,&g->numEdges);
+
+	for(i = 0; i < g->numVertexes; i++)
+	{
+		printf("please enter vertex %d:\n",i);
+		g->adjList[i].data = getchar();
+		g->adjList[i].firstedge = NULL;
+		while(g->adjList[i].data == '\n')
+		{
+			g->adjList[i].data = getchar();
+		}
+	}
+	for(k = 0;k<g->numEdges; k++)
+	{
+		printf("input edge<vi,vj>:\n");
+		char p,q;
+		p = getchar();
+		while(p == '\n')
+		{
+			p = getchar();
+		}
+		q = getchar();
+		while(q == '\n')
+		{
+			q = getchar();
+		}
+		int m,n;
+		m = Locate(g,p);
+		n = Locate(g,q);
+		if(m == -1|| n == -1)
+		{
+			return ;
+		}
+
+		e = (EdgeNode*)malloc(sizeof(EdgeNode));
+		if(e == NULL)
+		{
+			printf("malloc failed!\n");
+			return ;
+		}
+		e->adjvex = n;
+		e->next = g->adjList[i].firstedge;
+		g->adjList[m].firstedge = e;
+
+
+
+	}
+}
