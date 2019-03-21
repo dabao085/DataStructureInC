4cbd1b3f9475d331ecdd8b60181eb4baea2c0d64
chengxiang <chengxiang085@gmail.com>
2019-03-21 16:59:27 +0800

    use C++ 11

--- sort/mainTest.cpp
+++ sort/mainTest.cpp
@@ -9,11 +9,19 @@ using namespace std;
 int main()
 {
     vector<int> ivec = {9,7,5,3,1,8,6,4,2};
-    Sort<int> iSort(ivec);
-    iSort.bubbleSort();
-    
+    cout << "before sort: " << endl;
+    Sort<int> iSort1(ivec);
+    iSort1.bubbleSort();
+    cout << "after sort: " << endl;
     for(const auto &a : ivec)
         cout << a << " ";
     cout << endl;
+
+    cout << "before sort: " << endl;
+    Sort<int> iSort2({9,7,5,3,1,8,6,4,2});
+    iSort2.bubbleSort();
+    cout << "after sort: " << endl;
+    iSort2.printValues();
+
     return 0;
 }
--- sort/sort.h
+++ sort/sort.h
@@ -8,16 +8,29 @@
 
 #include <iostream>
 #include <vector>
+#include <utility>
 
 template <typename T>
 class Sort
 {
 public:
-	Sort(std::vector<T> &values) : m_values(values)//如何复制效率高?
-	{}
+	Sort(std::vector<T> &values) : m_values(values){printValues();}//如何复制效率高?
+	Sort(std::vector<T> &&values) : m_values(values)
+	{
+		std::cout << "rvalues constructor..." << std::endl;
+		printValues();
+	}
+
+	void printValues()
+	{
+		for(const auto &a : m_values)
+			std::cout << a << " ";
+		std::cout << std::endl;
+	}
 
 	void bubbleSort()
 	{
+		printValues();
 		T tmp;
 		int count = m_values.size();
 		if(count <= 0)
@@ -29,15 +42,18 @@ public:
 		{
 			for (int j = count - 1; j >= i; j--){
 				if (m_values[j] < m_values[j - 1]){
-					tmp = m_values[j];
-					m_values[j] = m_values[j - 1];
-					m_values[j - 1] = tmp;
+					// tmp = m_values[j];
+					// m_values[j] = m_values[j - 1];
+					// m_values[j - 1] = tmp;
+					std::swap(m_values[j], m_values[j - 1]);
 				}
 			}
 		}
 	}
+
 	void insertSort()
 	{
+		printValues();
 		T flag;
 		int count = m_values.size();
 		if(count == 0)
@@ -47,25 +63,27 @@ public:
 
 		for (int i = 1; i < count; ++i)//第一个值默认有序
 		{
-			flag = m_values[i];
+			flag = std::move(m_values[i]);
 			int j = i;
-			for (; (j > 0) && (flag < m_values[j]); --j)//Find the position of insertation
+			for (; (j > 0) && (flag < m_values[j - 1]); --j)//Find the position of insertation
 			{  
-				m_values[j] = m_values[j - 1];
+				m_values[j] = std::move(m_values[j - 1]);
 			}
-			m_values[j] = flag;
+			m_values[j] = std::move(flag);
 		}
 	}
+
 	void selectSort()
 	{
 		//Exchange with the smallest item in every round.
+		printValues();
 		int count = m_values.size();
 		if(count <= 0)
 		{
 			return ;
 		}
 
-		T min = m_values[0], tmp;
+		T min, tmp;
 		int index;
 		for (int i = 0; i < count; ++i)
 		{
@@ -78,14 +96,17 @@ public:
 					index = j;  //Get the index of the min
 				}
 			}
-			tmp = m_values[i];   //Exchange the min and the vecT[i];
-			m_values[i] = m_values[index];
-			m_values[index] = tmp;
+			// tmp = m_values[i];   //Exchange the min and the vecT[i];
+			// m_values[i] = m_values[index];
+			// m_values[index] = tmp;
+			std::swap(m_values[i], m_values[index]);
 		}
 	}
+
 	void heapSort();
 	void shellSort()
 	{
+		printValues();
 		int count = m_values.size();
 		if(count == 0)
 		{
@@ -96,13 +117,13 @@ public:
 		{
 			for(int i = gap; i < count; ++i)
 			{
-				T tmp = m_values[i];
+				T tmp = std::move(m_values[i]);
 				int j = i;
 				for(; j >= gap && tmp < m_values[j - gap]; j -= gap)
 				{
-					m_values[j] = m_values[j - gap];
+					m_values[j] = std::move(m_values[j - gap]);
 				}
-				m_values[j] = tmp;
+				m_values[j] = std::move(tmp);
 			}
 		}
 	}
